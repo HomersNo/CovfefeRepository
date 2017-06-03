32257efa5ec01853608c7c8d175a214793569f99
Jultrocor <j.troncosocorrea@gmail.com>
2017-06-03 14:05:48 +0200

    List controller covfefe

---
+++ Proyects/Acme-Chorbi/src/main/java/controllers/CovfefeController.java
@@ -0,0 +1,56 @@
+
+package controllers;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import services.CovfefeService;
+import domain.Covfefe;
+
+@Controller
+@RequestMapping("/covfefe")
+public class CovfefeController extends AbstractController {
+
+	//Services
+
+	@Autowired
+	private CovfefeService	covfefeService;
+
+
+	//Constructor
+
+	public CovfefeController() {
+		super();
+	}
+
+	//Listing
+
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public ModelAndView list(@RequestParam(required = false) final String errorMessage) {
+		ModelAndView result;
+
+		Collection<Covfefe> covfefes;
+
+		covfefes = this.covfefeService.findAll();
+
+		result = new ModelAndView("covfefe/list");
+		result.addObject("requestURI", "covfefe/list.do");
+		result.addObject("covfefes", covfefes);
+		result.addObject("errorMessage", errorMessage);
+
+		return result;
+	}
+
+	// Creation ---------------------------------------------------------------
+
+	// Edition ----------------------------------------------------------------
+
+	//Needs further testing
+
+}
